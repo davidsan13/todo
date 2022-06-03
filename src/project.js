de09459b1e563c04c2dbdb8fc7421adb5225bff3
davidsan13 <sandavid08@yahoo.com>
2022-06-02 20:21:02 -0700

    created module files

---
+++ src/project.js
@@ -0,0 +1,4 @@
+import task
+
+project 
+inheriet tasks
---
+++ src/task.js
@@ -0,0 +1,14 @@
+
+
+const Task = (title, date) => {
+  let done = false;
+
+  const getTitle = () => title;
+  const getDate = () => date;
+  isDone() {
+    done = true;
+  }
+  update
+  delete
+  return {getTitle, isDone, done}
+}
