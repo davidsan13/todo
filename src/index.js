773a3755829b5097e81f22af832d1af6987f0e8d
davidsan13 <sandavid08@yahoo.com>
2022-08-24 15:07:06 -0700

    webpack

--- src/index.js
+++ src/index.js
@@ -6,17 +6,11 @@ import Inbox from './components/inbox';
 import '@fortawesome/fontawesome-free/css/all.css';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import '@fortawesome/fontawesome-free/js/all';
-import Today from './components/today';
-import { formActive } from './components/form';
-
-
 
 function component() {
-  // element.appendChild(form());
   document.body.appendChild(renderLayout());
   Inbox();
   allListeners();
- 
+
 }
 component();
-// document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -27,25 +27,25 @@ module.exports = {
   },
   module: {
     rules: [
-      // {
-      //   test: /\.css$/i,
-      //   use: ['style-loader', 'css-loader'],
-      // },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
       {
         test: /\.(svg|eot|woff|woff2|ttf)$/,
         use: ['file-loader'],
       },
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          // Creates `style` nodes from JS strings
-          'style-loader',
-          // Translates CSS into CommonJS
-          'css-loader',
-          // Compiles Sass to CSS
-          'sass-loader',
-        ],
-      },
+      // {
+      //   test: /\.s[ac]ss$/i,
+      //   use: [
+      //     // Creates `style` nodes from JS strings
+      //     'style-loader',
+      //     // Translates CSS into CommonJS
+      //     'css-loader',
+      //     // Compiles Sass to CSS
+      //     'sass-loader',
+      //   ],
+      // },
     ],
   },
 };
