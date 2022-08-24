66ccb6b1f22fd151f6ec25d370058b03c777bed0
davidsan13 <sandavid08@yahoo.com>
2022-08-24 13:51:31 -0700

    webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -27,13 +27,24 @@ module.exports = {
   },
   module: {
     rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
+      // {
+      //   test: /\.css$/i,
+      //   use: ['style-loader', 'css-loader'],
+      // },
       {
         test: /\.(svg|eot|woff|woff2|ttf)$/,
         use: ['file-loader'],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
       },
     ],
   },
