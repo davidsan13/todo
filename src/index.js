272b11f03a9dbf8584d56d977737f66b088cb992
davidsan13 <sandavid08@yahoo.com>
2022-08-24 17:05:05 -0700

    remove fonts

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import { renderLayout } from './components/dom';
 import { allListeners } from './components/listeners';
 import Inbox from './components/inbox';
 // eslint-disable-next-line import/no-extraneous-dependencies
-import '@fortawesome/fontawesome-free/css/all.css';
+// import '@fortawesome/fontawesome-free/css/all.css';
 // eslint-disable-next-line import/no-extraneous-dependencies
-import '@fortawesome/fontawesome-free/js/all';
+// import '@fortawesome/fontawesome-free/js/all';
 
 function component() {
   document.body.appendChild(renderLayout());
